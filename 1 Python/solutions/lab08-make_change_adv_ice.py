07d70a07a97b2fcf9329289c92b22709e49bb588
DED8IRD <ewu72829@gmail.com>
2019-02-06 20:36:27 -0800

    handles change in all denominations using dictionary

--- 1 Python/solutions/lab08-make_change_adv_ice.py
+++ 1 Python/solutions/lab08-make_change_adv_ice.py
@@ -1,10 +1,10 @@
 # make_change_ice.py
 while True: # input validation
-    total_pennies = input('How much money do you have?: ').strip().strip('$')
+    total = input('How much money do you have?: ').strip().strip('$')
     try: 
         # this will give you a ValueError if you try to turn a string into a number
-        total_pennies = round(float(total_pennies) * 100)
-        if total_pennies < 0:
+        total = round(float(total) * 100)
+        if total < 0:
             raise ValueError
         break # if no error occurs, we break out of the inf loop and continue in our program
     except ValueError: # we catch and print an error message
@@ -25,9 +25,9 @@ denomination = {
 
 change = ['Your change is']
 
-for d in denomination:
-    d_change = total_pennies // denomination[d]
-    total_pennies %= denomination[d]
-    change.append(f'{round(d_change)} {d}')
+for d in denomination: # loop through all denominations (d is key)
+    d_change = total // denomination[d] # get change by floor dividing total with denom
+    total %= denomination[d] # remove denomination from total
+    change.append(f'{round(d_change)} {d}') # add change + denom name to change list
 
-print(', '.join(change))
+print(', '.join(change)) # print final change
