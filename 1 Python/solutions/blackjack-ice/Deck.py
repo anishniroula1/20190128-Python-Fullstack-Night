01c1aeeaba941c208bd88608e99c1435179e4ac4
DED8IRD <ewu72829@gmail.com>
2019-02-27 21:15:51 -0800

    completed blackjack lab

--- 1 Python/solutions/blackjack-ice/Deck.py
+++ 1 Python/solutions/blackjack-ice/Deck.py
@@ -9,9 +9,8 @@ class Card:
     """
     Card represented by suit and rank
     """
-
     def __init__(self, rank, suit):
-        points = {'A': 1}
+        points = {'A': 11}
         nums = dict(zip(range(2,11), range(2,11)))
         face = {k: 10 for k in 'JQK'} 
         points.update(nums)
@@ -76,4 +75,4 @@ class Deck:
         """
         pops card off top of deck
         """
-        return self.cards.pop()        
+        return self.cards.pop()
