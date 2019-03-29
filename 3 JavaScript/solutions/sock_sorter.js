4c7475573e8e550a013183ac333755f8e1d8237b
DED8IRD <ewu72829@gmail.com>
2019-03-28 21:07:30 -0700

    socksorter part 2 solution

--- 3 JavaScript/solutions/sock_sorter.js
+++ 3 JavaScript/solutions/sock_sorter.js
@@ -1,11 +1,14 @@
 // sock_sorter.js
 const sockTypes = ['ankle', 'crew', 'calf', 'thigh']
+const sockColors = ['black', 'white', 'blue']
 const sockList = []
 const sockCounter = {}
 
 for (let i=0; i<100; i++) {
   // randomly select sock from socktypes
-  let sock = sockTypes[Math.floor(Math.random() * sockTypes.length)]
+  let type = sockTypes[Math.floor(Math.random() * sockTypes.length)]
+  let color = sockColors[Math.floor(Math.random() * sockColors.length)]
+  let sock = [type, color]
   sockList.push(sock)
 
   // setting default values: equivalent to dict.get() in JS 
@@ -19,7 +22,7 @@ for (let i=0; i<100; i++) {
 }
 
 for (let sock in sockCounter) {
-  console.log(`${sock} has ${sockCounter[sock] % 2} loner(s).`)
+  console.log(`(${sock}) has ${sockCounter[sock] % 2} loner(s).`)
 }
 
 console.log(sockList)
