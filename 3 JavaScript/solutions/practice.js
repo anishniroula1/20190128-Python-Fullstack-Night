97040ce00f64d72ec78d4baa6d8c087b1516a0ae
DED8IRD <ewu72829@gmail.com>
2019-04-16 18:47:25 -0700

    fizzbuzz

--- 3 JavaScript/solutions/practice.js
+++ 3 JavaScript/solutions/practice.js
@@ -310,3 +310,58 @@ function unify(d) {
     }
     return avg
 }
+
+// FizzBuzz
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print "Fizz" instead of the number 
+// and for multiples of five print "Buzz". 
+// For numbers which are multiples of both three and five print "FizzBuzz".
+function fizzbuzz(n) {
+    for (let i=1; i<=n; i++) {
+        if (i % 15 === 0) console.log('FizzBuzz') 
+        else if (i % 3 === 0) console.log('Fizz') 
+        else if (i % 5 === 0) console.log('Buzz')
+        else console.log(i)
+    }
+}
+
+function fizzbuzz(n) {
+    for (let i=1; i<=n; i++) {
+        let num = ''
+        if (i % 3 === 0) num += 'Fizz' 
+        if (i % 5 === 0) num += 'Buzz'
+        console.log(num ? num : i)
+    }
+}
+
+// Advanced version:
+// Now try this with multiples of 7 as 'fuzz' 
+// and multiples of 9 as 'baz' in addition to 'fizz' and 'buzz'.
+function fizzBuzzFuzzBaz(n) {
+    for (let i=1; i<n+1; i++) {
+        let num = ''
+        if (i % 3 === 0) num += 'Fizz' 
+        if (i % 5 === 0) num += 'Buzz'
+        if (i % 7 === 0) num += 'Fuzz'
+        if (i % 9 === 0) num += 'Baz'
+        console.log(num ? num : i)
+
+        // // equivalent to above ternary
+        // if (num) {
+        //     console.log(num)
+        // } else {
+        //     console.log(i)
+        // }
+    }
+}
+
+function fizzBuzzFuzzBaz(n) {
+    const translate = {3: 'Fizz', 5: 'Buzz', 7: 'Fuzz', 9: 'Baz'}
+    for (let i=1; i<=n; i++) {
+        let num = ''
+        for (let k in translate) {
+            if (i % k === 0) num += translate[k]
+        }
+        console.log(num ? num : i)    
+    }
+}
