a07ada0f2c62d5a1c3b5e7359ce6c27691ad5e4c
DED8IRD <ewu72829@gmail.com>
2019-04-25 19:37:45 -0700

    todos wip

--- 4 Django/solutions-1-28/todos/todosite/todos/views.py
+++ 4 Django/solutions-1-28/todos/todosite/todos/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('pls work')
+    todos = Todo.objects.all().order_by('-created_date', 'completed')
+    return render(request, 'todos/index.html', {'todos': todos})
 
 def add_todo(request):
-    pass
+    # print(request)
+    # print(request.POST)
+    if request.method == 'POST':
+        # create new todo from POST parameters
+        text_from_input = request.POST['todo']
+        todo = Todo(text=text_from_input)
+        todo.save()
+    return redirect('todos:index')
 
 def toggle_todo(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
+    # # equivalent to above, but get_obj_or_404 is safer
     # todo = Todo.objects.get(pk=pk)
     todo.toggle()
     return HttpResponse('success')
